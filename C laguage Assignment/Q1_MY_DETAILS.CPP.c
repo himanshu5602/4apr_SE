538da9e3445f5d0a85096071a5d81cf4f6905d5b
himanshu5602 <130464068+himanshu5602@users.noreply.github.com>
2023-05-12 13:29:28 +0530

    Add files via upload

---
+++ C laguage Assignment/Q1_MY_DETAILS.CPP.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	printf("My name is Ashutosh Singh");
+	printf("\nI was born on 17 march 2001");
+	printf("\nI am 22 years old");
+	printf("\nI stay in naranpura,ahmedabad");
+	
+}
